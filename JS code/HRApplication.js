32f0a18b08f59aac3781b9abfb8d50db93919332
John Meissner <johnmeissner@Johns-MacBook-Pro.local>
2021-10-26 17:26:18 +0100

    add function added

--- JS code/HRApplication.js
+++ JS code/HRApplication.js
@@ -1,12 +1,34 @@
-let jsonData = "https://jsonplaceholder.typicode.com/posts/1";
-fetch(jsonData)
-    .then((response) => {
-        if (response.status !== 200) {
-            console.log("There was a problem");
-            return;
-        }
 
-        response.json().then((data) => {
-            console.log(data);
+
+
+
+//add 
+
+//event listener on submit button, once button is clicked need to add form fields into JSON
+const addforminput = document.querySelectorAll(`ninumberinput, nameinput, phonenumberinput, addressinput, departmentinput`);
+
+addforminput.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch(//add URL?, 
+        {
+            method: `post`,
+            body: formData
+        }).then(function (response) {
+            return response.text();
+        }).then(function (text) {
+            console.log(text);
+        }).catch(function (error) {
+            console.error(error);
         })
-    })
+
+
+});
+
+//delete
+
+
+
+
