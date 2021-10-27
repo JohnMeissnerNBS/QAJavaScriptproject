819025af2b8a8895183cee2f76725c6030c5039f
John Meissner <johnmeissner@Johns-MacBook-Pro.local>
2021-10-27 16:10:06 +0100

    updated add function

--- JS code/HRApplication.js
+++ JS code/HRApplication.js
@@ -1,33 +1,58 @@
 
 
+//read
 
 
-//add 
+//const jsonrecords = "recordsJSON";
 
-//event listener on submit button, once button is clicked need to add form fields into JSON
-const addforminput = document.querySelectorAll(`ninumberinput, nameinput, phonenumberinput, addressinput, departmentinput`);
+//fetch(recordsJSON)
+//  .then(response => console.log(response));
 
-addforminput.addEventListener("click", function (e) {
+//add
+
+
+
+function pushData() {
+    var recordinputs = document.getElementById("ninumberinput", "nameinput", "phonenumberinput", "addressinput", "departmentinput").value;
+
+
+    recordsJSON.push(recordinputs);
+}
+
+submit.addEventListener("click", pushData(), (e) => {
     e.preventDefault();
-
-    const formData = new FormData(this);
-
-    fetch(//add URL?, 
-        {
-            method: `post`,
-            body: formData
-        }).then(function (response) {
-            return response.text();
-        }).then(function (text) {
-            console.log(text);
-        }).catch(function (error) {
-            console.error(error);
-        })
-
+    console.log("error");
 
 });
 
+
+
+
 //delete
+
+//JSON
+
+var recordsJSON =
+
+    [
+        {
+            "NI Number": "1111111111",
+            "Full Name": "John Meissner",
+            "Phone Number": "01793 1111111",
+            "Address": "1 null street, nulltown, N11 1NN",
+            "Department": "IT"
+        },
+
+        {
+            "NI Number": "22222222222",
+            "Full Name": "Bob Smith",
+            "Phone Number": "01793 22222222",
+            "Address": "2 null street, nulltown, N22 2NN",
+            "Department": "HR"
+        },
+
+    ]
+
 
 
 
